6a2ee23bcf1a290960470aa705acefcc56b83696
Chris Collins <chrisc@vrlaw.com.au>
2012-09-17 15:30:03 +1000

    Revert "slowly work the bits together"
    This reverts commit a20286702b1505b6fdebe7394f738fd59fe10387.

--- debian/flashcache.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the flashcache package
-#
-0 4	* * *	root	[ -x /usr/bin/flashcache_maintenance ] && /usr/bin/flashcache_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(flashcache):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="flashcache" command="/usr/bin/flashcache"
